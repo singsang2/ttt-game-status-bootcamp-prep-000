4bdd43b47256719aeebfd6566e67a3757a56ef7a
Sung Bae <singsang2@gmail.com>
2018-09-30 09:33:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if (board[win_combination[0]]==board[win_combination[1]] && board[win_combination[1]]==board[win_combination[2]])&&position_taken(board,win_combination[0])
+    if (board[win_combination[0]]==board[win_combination[1]] && board[win_combination[1]]==board[win_combination[2]])&& position_taken(board,win_combination[0])
       return win_combination
     else
       return false
