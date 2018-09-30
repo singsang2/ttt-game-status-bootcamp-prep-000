6c62297b5917bc425d8cef49def64b61dd43d320
Sung Bae <singsang2@gmail.com>
2018-09-30 09:25:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,9 @@ WIN_COMBINATIONS = [
   [2, 5, 8]]
 
 def won?(board)
-  
+  WIN_COMBINATIONS.each do |win_combination|
+    if board[win_combination[0]]==board[win_combination[1]] && board[win_combination[1]]==board[win_combination[2]]
+      return win_combination
 end
 
 def full?(board)
